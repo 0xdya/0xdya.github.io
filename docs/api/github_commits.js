ca6d492b31b0f926c149a563a500506e34acc290
0xdya <178745289+0xdya@users.noreply.github.com>
2025-06-30 23:27:06 +0100

    Update github_commits.js

--- docs/api/github_commits.js
+++ docs/api/github_commits.js
@@ -1,25 +1,20 @@
 export default async function handler(req, res) {
-  const token = process.env.GITHUB_token; 
-  const username = "0xdya";
+  const token = process.env.GITHUB_TOKEN;
   const repo = "0xdya.github.io";
+  const username = "0xdya";
   const branch = "main";
-  const perPage = 30;
+  const perPage = 100;
 
-  const url = `https://api.github.com/repos/${username}/${repo}/commits?sha=${branch}&per_page=${perPage}`;
-
-  try {
-    const response = await fetch(url, {
+  const response = await fetch(
+    `https://api.github.com/repos/${username}/${repo}/commits?sha=${branch}&per_page=${perPage}`,
+    {
       headers: {
         Authorization: `Bearer ${token}`,
-        Accept: "application/vnd.github+json"
-      }
-    });
+        Accept: "application/vnd.github+json",
+      },
+    }
+  );
 
-    const data = await response.json();
-    res.status(200).json(data);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "فشل جلب الكوميتات" });
-  
-  }
+  const data = await response.json();
+  res.status(200).json(data);
 }
