6a0d3378c2501b0a30b10ef6e85b6cef55d64ce3
0xdya <0xdya@proton.me>
2026-03-18 15:16:28 +0100

    استخدام preloader فقط فالصفحة الرئيسية

--- docs/js/loader.js
+++ docs/js/loader.js
@@ -1,6 +1,6 @@
-window.addEventListener('load', () => {
-    const loaderWrapper = document.getElementById('loader-wrapper');
-    if (loaderWrapper) {
-        loaderWrapper.classList.add('loader-hidden');
-    }
-});
+// window.addEventListener('load', () => {
+//     const loaderWrapper = document.getElementById('loader-wrapper');
+//     if (loaderWrapper) {
+//         loaderWrapper.classList.add('loader-hidden');
+//     }
+// });
